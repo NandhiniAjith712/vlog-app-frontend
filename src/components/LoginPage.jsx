d2a08ddadf0451cfc05425938ceda185dec08b3b
NandhiniAjith712 <nandhu7246@gmail.com>
2024-02-10 11:00:13 +0530

    updated login

---
+++ src/components/LoginPage.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div> 
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <div className="row g-3">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="form-label">E-mail</label>
+                        <input type="email" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label htmlFor="" className="form-label">Password</label>
+                        <input type="password" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <button className="btn btn-success">LOGIN</button>
+                    </div>
+                    <a href=''>New users click here</a>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default LoginPage
